a0851e42f6876d277b017813c692330864842e1b
Brian Corrigan <brian@madgloryint.com>
2015-08-21 12:50:04 -0400

    Adds initial commit

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
-# compression plugin for webpack
+# add byte order mark plugin for webpack
 
 ## Usage
 
 ``` javascript
-var CompressionPlugin = require("compression-webpack-plugin");
+var AddBomPlugin = require("addbom-webpack-plugin");
 module.exports = {
 	plugins: [
-		new CompressionPlugin({
-			asset: "{file}.gz",
-			algorithm: "gzip",
-			regExp: /\.js$|\.html$/,
-			threshold: 10240,
-			minRatio: 0.8
+		new AddBomPlugin({
+			regExp: /\.js$|\.html$/
 		})
 	]
 }
@@ -19,11 +15,7 @@ module.exports = {
 
 Arguments:
 
-* `asset`: The target asset name. `{file}` is replaced with the original asset. Defaults to `"{file}.gz"`.
-* `algorithm`: Can be a `function(buf, callback)` or a string. For a string the algorithm is tacken from `zlib`. Defaults to `"gzip"`.
 * `regExp`: All assets matching this RegExp are processed. Defaults to every asset.
-* `threshold`: Only assets bigger than this size are processed. In bytes. Defaults to `0`.
-* `minRatio`: Only assets that compress better that this ratio are processed. Defaults to `0.8`.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,45 +1,27 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-var async = require("async");
+var async = require('async');
+var RawSource = require('webpack/lib/RawSource');
+var BOM = new Buffer('\uFEFF');
 
-var RawSource = require("webpack/lib/RawSource");
-
-function CompressionPlugin(options) {
+function AddBomPlugin(options) {
 	options = options || {};
-	this.asset = options.asset || "{file}.gz";
-	this.algorithm = options.algorithm || "gzip";
-	if(typeof this.algorithm === "string") {
-		var zlib = require("zlib");
-		this.algorithm = zlib[this.algorithm];
-		if(!this.algorithm) throw new Error("Algorithm not found in zlib");
-		this.algorithm = this.algorithm.bind(zlib);
-	}
 	this.regExp = options.regExp;
-	this.threshold = options.threshold || 0;
-	this.minRatio = options.minRatio || 0.8;
 }
-module.exports = CompressionPlugin;
+module.exports = AddBomPlugin;
 
-CompressionPlugin.prototype.apply = function(compiler) {
-	compiler.plugin("this-compilation", function(compilation) {
-		compilation.plugin("optimize-assets", function(assets, callback) {
-			async.forEach(Object.keys(assets), function(file, callback) {
-				if(this.regExp && !this.regExp.test(file)) return callback();
+AddBomPlugin.prototype.apply = function (compiler) {
+	compiler.plugin('this-compilation', function (compilation) {
+		compilation.plugin('optimize-assets', function (assets, callback) {
+			async.forEach(Object.keys(assets), function (file, callback) {
+				if (this.regExp && !this.regExp.test(file)) {
+					return callback();
+				}
 				var asset = assets[file];
 				var content = asset.source();
-				if(!Buffer.isBuffer(content))
-					content = new Buffer(content, "utf-8");
-				var originalSize = content.length;
-				if(originalSize < this.threshold) return callback();
-				this.algorithm(content, function(err, result) {
-					if(err) return callback(err);
-					if(result.length / originalSize > this.minRatio) return callback();
-					var newFile = this.asset.replace(/\{file\}/g, file);
-					assets[newFile] = new RawSource(result);
-					callback();
-				}.bind(this));
+				if (!Buffer.isBuffer(content)) {
+					content = new Buffer(content, 'utf-8');
+				}
+				var contents = Buffer.concat([BOM, file.contents]);
+				assets[file] = new RawSource(contents);
 			}.bind(this), callback);
 		}.bind(this));
 	}.bind(this));
